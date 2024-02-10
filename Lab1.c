67ca557ac69a1ca8f57b0e98eaf6dd682e5f72c6
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-02-10 15:49:11 +1000

    Create Lab1.c

---
+++ Lab1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a, b, c;
+    double d;
+    double x1, x2;
+    
+    a = 5.1;
+    b = 1;
+    c = -2.2;
+    
+    d = b * b - 4 * a * c;
+    printf("d = %lf\n", d);
+    
+    if (d > 0) {
+        x1 = ((-b + sqrt(d))/(2 * a));
+        x2 = ((-b - sqrt(d))/(2 * a));
+        printf("x1 = %lf x2 = %lf", x1, x2);
+    }
+    
+    if (d == 0) {
+        x1 = (-b)/(2 * a);
+        printf("x1 = %lf", x1);
+    }
+    
+    if (d < 0) {
+        printf("Корней нет");
+    }
+}
