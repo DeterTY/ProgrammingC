7992746e3d477c95cf887b13f6eefb04d909dc39
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-02-10 16:26:30 +1000

    Update Lab1.c

--- Lab1.c
+++ Lab1.c
@@ -12,32 +12,37 @@ int main()
     b = 1;
     c = -2.2;
     
-    do {
-    printf("Введите a:");
+    do 
+    {
+    printf("Введите a: ");
     scanf("%lf", &a);
-    printf("Введите b:");
+    printf("Введите b: ");
     scanf("%lf", &b);
-    printf("Введите c:");
+    printf("Введите c: ");
     scanf("%lf", &c);
     
     d = b * b - 4 * a * c;
     printf("d = %lf\n", d);
     
-    if (d > 0) {
+    if (d > 0) 
+        {
         x1 = ((-b + sqrt(d))/(2 * a));
         x2 = ((-b - sqrt(d))/(2 * a));
         printf("x1 = %lf x2 = %lf", x1, x2);
-    }
+        }
     
-    if (d == 0) {
+    if (d == 0) 
+        {
         x1 = (-b)/(2 * a);
         printf("x1 = %lf", x1);
-    }
+        }
     
-    if (d < 0) {
+    if (d < 0) 
+        {
         printf("Корней нет\n");
-    }
+        }
     printf("Продолжить? 1 - да, 2 - нет: ");
     scanf("%d", &i);
-    } while (i != 2);
+    } 
+    while (i != 2);
 }
