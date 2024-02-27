47fe6a6c1196a35fa167202e709b9544073fd1ae
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-02-27 20:52:36 +1000

    Update Lab3.c

--- Lab3.c
+++ Lab3.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main()
+{
+    char s[80];
+    fread(s, sizeof(s));???
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] == 'a')
+            s[i] = 'A';
+        else if (s[i] == 'b')
+            s[i] = 'B';
+        i++;
+    }
+    printf("%s\n", s);
+}
