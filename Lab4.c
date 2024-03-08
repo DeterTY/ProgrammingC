d6e372291450372760d121bec0027c600ec97f24
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-03-08 18:28:47 +1000

    Update Lab4.c

--- Lab4.c
+++ Lab4.c
@@ -1,7 +1,32 @@
-ЗАДАНИЕ 4
-Написать функции, вычисляющие периметр и площадь треугольника по
-значениям длин трёх его сторон. Разместить их в отдельном модуле (отличном от того,
-где располагается функция main( )). Ввести с консоли значения длин сторон
-треугольника (с проверкой на корректность неравенства треугольника) и выдать на
-консоль вычисленные с помощью написанных функций значения его периметра и
-площади.
+#include <stdio.h>
+#include <math.h>
+
+double Perimetr(double a, double b, double c) 
+{
+    return a + b + c;
+}
+
+double Square(double a, double b, double c) 
+{
+    double square = Perimetr(a, b, c) / 2;
+    return sqrt(square * (square - a) * (square - b) * (square - c));
+}
+
+int main() 
+{
+    double a, b, c;
+    printf("Введите длину для стороны треугольника A: ");
+    scanf("%lf", &a);
+    printf("Введите длину для стороны треугольника B: ");
+    scanf("%lf", &b);
+    printf("Введите длину для стороны треугольника C: ");
+    scanf("%lf", &c);
+    if (a + b > c && a + c > b && b + c > a) 
+    {
+        double perimeter = Perimetr(a, b, c);
+        double area = Square(a, b, c);
+        printf("Периметр = %lf\n", Perimetr);
+        printf("Площадь = %lf\n", Square);
+    }
+    return 0;
+}
