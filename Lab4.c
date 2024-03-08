e2f2c2a0c14bbdea2fc30d154f7adcb4de6331f3
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-03-09 09:00:49 +1000

    Update Lab4.c

--- Lab4.c
+++ Lab4.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
-double Perimetr(double a, double b, double c) 
+double P(double a, double b, double c)
 {
-    return a + b + c;
+    return (a + b + c);
 }
 
-double Square(double a, double b, double c) 
+double S(double a, double b, double c)
 {
-    double square = Perimetr(a, b, c) / 2;
-    return sqrt(square * (square - a) * (square - b) * (square - c));
+    return (sqrt((P(a, b, c) / 2) * ((P(a, b, c) / 2) - a) * ((P(a, b, c) / 2) - b) * ((P(a, b, c) / 2) - c)));
 }
 
-int main() 
+int main()
 {
     double a, b, c;
+    
     printf("Введите длину для стороны треугольника A: ");
     scanf("%lf", &a);
     printf("Введите длину для стороны треугольника B: ");
     scanf("%lf", &b);
     printf("Введите длину для стороны треугольника C: ");
     scanf("%lf", &c);
-    if (a + b > c && a + c > b && b + c > a) 
-    {
-        double perimeter = Perimetr(a, b, c);
-        double area = Square(a, b, c);
-        printf("Периметр = %lf\n", Perimetr);
-        printf("Площадь = %lf\n", Square);
-    }
+    
+    double Perimeter = P(a, b, c);
+    double Square = S(a, b, c);
+    printf("Периметр = %lf\n", Perimeter);
+    printf("Площадь = %lf\n", Square);
+
     return 0;
 }
