c616df82db559a74474b54dbabc409bf094d81e0
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-03-08 21:51:09 +1000

    Update Lab3.c

--- Lab3.c
+++ Lab3.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main() 
+{
     char str[80];
     int i;
+    printf("Введите строку (<= 80 символов): ");
     fgets(str, 80, stdin);
-    for (i = 0; str[i] != '\0'; i++) 
+    printf("Программа заменяет все маленькие символы 'a' и 'b' на заглавные.\n");
+    for (i = 0; i < 81; i++) 
     {
         if(str[i] == 'a' or str[i] == 'b') 
         {
             str[i] = str[i] - 32;
         }
     }
-    printf("%s", str);
+    printf("Полученная строка: %s", str);
     return 0;
 }
