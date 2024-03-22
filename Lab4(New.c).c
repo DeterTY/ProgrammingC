ae6f3e1a4a5a5721f308b1a37266fe28f6fb6f39
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-03-23 08:57:47 +1000

    Update Lab4(New.c).c

--- Lab4(New.c).c
+++ Lab4(New.c).c
@@ -1,1 +1,17 @@
+#include <math.h>
+#include <stdbool.h>
 
+double P(double a, double b, double c)
+{
+    return (a + b + c);
+}
+
+double S(double a, double b, double c)
+{
+    return (sqrt((P(a, b, c) / 2) * ((P(a, b, c) / 2) - a) * ((P(a, b, c) / 2) - b) * ((P(a, b, c) / 2) - c)));
+}
+
+bool E(double a, double b, double c)
+{
+    return ((a + b > c) && (a + c > b) && (b + c > a));
+}
