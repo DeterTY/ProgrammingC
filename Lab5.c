4790331bd47e073b3d7d8165a3d22918b422b4f9
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-03-22 21:18:09 +1000

    Update Lab5.c

--- Lab5.c
+++ Lab5.c
@@ -1,64 +1,107 @@
+#Основа. main.c
+    
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include "m1m2.h"
 
-int main()
-{
+int main() {
     int n;
     printf("Введите размерность матриц: ");
     scanf("%d", &n);
-    double** m1;
-    double** m2;
-    m1 = (double**)malloc(n * sizeof(double*));
-    int i, j;
-    for(i = 0; i < n; i++)
+
+    double* m1 = (double*)malloc(n * n * sizeof(double));
+    double* m2 = (double*)malloc(n * n * sizeof(double));
+
+    for(int i = 0; i < n; i++) 
     {
-        for(j = 0; j < n; j++)
+        for(int j = 0; j < n; j++) 
         {
             printf("m1[%d][%d] = ", i + 1, j + 1);
-            scanf("%lf", &m1[i][j]);
+            scanf("%lf", &m1[i * n + j]);
         }
     }
-    for(i = 0; i < n; i++)
+
+    for(int i = 0; i < n; i++) 
     {
-        for(j = 0; j < n; j++)
+        for(int j = 0; j < n; j++) 
         {
-            printf("%lf ", m1[i][j]);
+            printf("%lf ", m1[i * n + j]);
         }
         printf("\n");
     }
-    m2 = (double**)malloc(n * sizeof(double*));
-    for(i = 0; i < n; i++)
+
+    for(int i = 0; i < n; i++) 
     {
-        for(j = 0; j < n; j++)
+        for(int j = 0; j < n; j++) 
         {
             printf("m2[%d][%d] = ", i + 1, j + 1);
-            scanf("%lf", &m1[i][j]);
+            scanf("%lf", &m2[i * n + j]);
         }
     }
-    for(i = 0; i < n; i++)
+
+    for(int i = 0; i < n; i++) 
     {
-        for(j = 0; j < n; j++)
+        for(int j = 0; j < n; j++) 
         {
-            printf("%lf ", m1[i][j]);
+            printf("%lf ", m2[i * n + j]);
         }
         printf("\n");
     }
-    char f;
-    double* M;
-    M = (double*)malloc(n * n * sizeof(double));
+
+    char math;
     printf("Введите операцию +, -, *: ");
-    scanf(" %c", &f);
-    if(f = '+')
+    scanf(" %c", &math);
+
+    double* M = f(m1, m2, n, math);
+
+    for(int i = 0; i < n; i++) 
     {
-        for(i = 0; i < n; i++)
+        for(int j = 0; j < n; j++) 
         {
-            for(j = 0; j < n; j++)
+            printf("%lf ", M[i * n + j]);
+        }
+        printf("\n");
+    }
+
+    free(m1);
+    free(m2);
+    free(M);
+
+    return 0;
+}
+
+# m1m2.h
+
+double* f(double* m1, double* m2, int n, char math);
+
+# m1m2.c
+
+#include "m1m2.h"
+#include <malloc.h>
+
+double* f(double* m1, double* m2, int n, char math) 
+{
+    double* M = (double*)malloc(n * n * sizeof(double));
+
+    for(int i = 0; i < n; i++) 
+    {
+        for(int j = 0; j < n; j++) 
+        {
+            if(math == '+') 
             {
-                
+                M[i * n + j] = m1[i * n + j] + m2[i * n + j];
+            } 
+            else if(math == '-') 
+            {
+                M[i * n + j] = m1[i * n + j] - m2[i * n + j];
+            } 
+            else if(math == '*') 
+            {
+                M[i * n + j] = m1[i * n + j] * m2[i * n + j];
             }
-            printf("\n");
         }
     }
-    return 0;
+
+    return M;
 }
