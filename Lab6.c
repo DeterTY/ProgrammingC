b19c8ab44be2259fc45de58b58ddbfe3bd3303a4
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-03-22 21:22:19 +1000

    Update Lab6.c

--- Lab6.c
+++ Lab6.c
@@ -13,23 +13,6 @@ int main() {
     char first_name[50], last_name[50], middle_name[50];
     int birth_year;
 
-    // Открываем файл для чтения
-    input_file = fopen("input.txt", "r");
-    if(input_file == NULL) 
-    {
-        printf("Ошибка при открытии файла для чтения.\n");
-        return 1;
-    }
-
-    // Открываем файл для записи
-    output_file = fopen("output.txt", "w");
-    if(output_file == NULL) 
-    {
-        printf("Ошибка при открытии файла для записи.\n");
-        return 1;
-    }
-
-    // Считываем данные из файла и записываем нужные строки в другой файл
     while(fscanf(input_file, "%s %s %s %d", last_name, first_name, middle_name, &birth_year) == 4) 
     {
         if(birth_year > 1980) 
@@ -38,7 +21,6 @@ int main() {
         }
     }
 
-    // Закрываем файлы
     fclose(input_file);
     fclose(output_file);
 
